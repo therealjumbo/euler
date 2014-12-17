77e04fb68ebd0a9c3474ad2e0e69c5327f835d6a
Jeff Zignego <jlzignego@gmail.com>
2014-12-17 17:48:06 -0600

    added solution to problem 20 in python 2

---
+++ prob20/prob20.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 05 12:50:08 2012
+
+@author: n3wp0llut10n
+"""
+from math import *
+
+num = 0
+for x in str(factorial(100)):
+    num += int(x)
+    
+print num
