a148c95b39df32289731243c5dafc3d07afcafcc
Jeff Zignego <jlzignego@gmail.com>
2014-12-18 23:49:43 -0600

    converted problem 13 to python 3, fixed problem 13, was incorrect and incoherent, and deleted duplicate data file

--- prob13/prob13.py
+++ prob13/prob13.py
@@ -5,47 +5,26 @@ Created on Thu Nov 01 18:55:41 2012
 
 @author: n3wp0llut10n
 """
+import functools
 
 f = open('data.txt')
 
 string_list = []
-
 for line in f:
     string_list.append(line)
 
 f.close()
 
-num_list = [list(i.replace('\n', '')) for i in string_list]
+num_list = [(i.replace('\n', '')) for i in string_list]
 
-num_list = [map(int, num_list[i]) for i in range(0, len(num_list))]
-
-"""
-the immediately preceding comprehension is equivalent to this loop
-which one is more readable?
 for i in range(0, len(num_list)):
-    for j in range(0, len(num_list[i])):
-        num_list[i][j] = int(num_list[i][j])
-"""
+    num_list[i] = int(num_list[i])
 
-num_list_tr = map(list, zip(*num_list))
+total = 0
+for i in reversed(num_list):
+   total += i
 
-temp = 0
-temp_str = ""
-stack = []
-for j in reversed(num_list_tr):
-    temp = temp + sum(j)
-    temp_str = str(temp)
-    stack.append(int(temp_str[-1]))
-    temp = int(temp_str[:-1])
+total_str = str(total)
 
-temp_str = temp_str[:-1]
-for i in range(0, len(temp_str)) :
-    stack.append( int( temp_str[len(temp_str) - 1 - i] ) )
-    
-print stack
-new_str = ""
 for i in range(0, 10):
-    new_str += str(stack.pop())
-    
-print new_str
-
+    print(total_str[i])
