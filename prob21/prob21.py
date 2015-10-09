14152fe3150270a5ad3045dd67359609895dca02
Jeff Zignego <jlzignego@gmail.com>
2015-10-08 21:47:43 -0500

    prob21 additions

--- prob21/prob21.py
+++ prob21/prob21.py
@@ -5,4 +5,15 @@ sys.path.append("..")
 
 import euler.mymath
 
+for i in range(1, 10000):
+    all_amicables = []
+    a = i
+    b = sum(euler.mymath.proper_divisors(a))
+    
+        all_amicables.append((a, b))
+    
+total = 0
+for amicable in all_amicables:
+    total += amicable[0] + amicable[1]
 
+print(total)
