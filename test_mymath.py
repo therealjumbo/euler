d86d8e3f76b3314564064961204fe1830520a7e6
Jeff Zignego <jlzignego@gmail.com>
2015-10-09 19:08:38 -0500

    tests more comprehensive

--- test_mymath.py
+++ test_mymath.py
@@ -1,11 +1,14 @@
 from euler.mymath import is_prime
 
-def test_is_prime_corner_cases():
-    assert is_prime(0) == False
-    assert is_prime(1) == False 
-    assert is_prime(2) == True
-    assert is_prime(3) == True
-    assert is_prime(4) == False
+def test_is_prime_first_100_non_primes():
+    non_primes = [0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25,
+    26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40, 42, 44, 45, 46, 48, 49, 50,
+    51, 52, 54, 55, 56, 57, 58, 60, 62, 63, 64, 65, 66, 68, 69, 70, 72, 74, 75,
+    76, 77, 78, 80, 81, 82, 84, 85, 86, 87, 88, 90, 91, 92, 93, 94, 95, 96, 98, 
+    99, 100, 102, 104, 105, 106, 108, 110, 111, 112, 114, 115, 116, 117, 118, 
+    119, 120, 121, 122, 123, 124, 125, 126, 128, 129, 130]
+    for non_prime in non_primes:
+        assert is_prime(non_prime) == False
 
 def test_is_prime_first_100_primes():
     primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,
