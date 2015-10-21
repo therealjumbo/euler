d15fc5ebd3f8f1aac65067bfa76e32474e3d1c25
Jeff Zignego <jlzignego@gmail.com>
2015-10-21 15:16:53 -0500

    completed prob22

--- prob22/prob22.py
+++ prob22/prob22.py
@@ -3,7 +3,17 @@
 f = open("p022_names.txt", 'r')
 text = f.read()
 
-names_list = text.split(',')
+names_list = text.replace('"', '').split(',')
+names_list = [name.lower() for name in names_list]
+names_list.sort()
 
+total_value = 0
+for position, name in enumerate(names_list):
+    alpha_value = 0
+    for char in name:
+       alpha_value += ord(char) - ord('a') + 1 
+    total_value += (position + 1) * alpha_value
+    
+print(total_value)
 
 f.close()
