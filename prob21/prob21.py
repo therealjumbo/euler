9768ea4f6d3602e02f3cb6dfcecef9c43e569564
Jeff Zignego <jlzignego@gmail.com>
2015-10-21 12:32:18 -0500

    moved a check from prob21 into mymath

--- prob21/prob21.py
+++ prob21/prob21.py
@@ -7,7 +7,7 @@ import euler.mymath
 all_amicables = []
 for a in range(1, 10000):
     b = sum(euler.mymath.proper_divisors(a))
-    if euler.mymath.is_amicable(a, b) == True and (a != b):
+    if euler.mymath.is_amicable(a, b):
         if (b, a) not in all_amicables: 
             all_amicables.append((a, b))
     
